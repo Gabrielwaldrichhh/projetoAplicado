89a140e98f0d9128f03c223e7a28a5f07df88c31
GabrielWaldrich <gilberto-waldrich@hotmail.com>
2023-11-23 19:10:29 -0300

    inicial
    inicial

--- easy-party/src/index.js
+++ easy-party/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
-import Index from './Pages/PageMap/Index' 
 import './Pages/Styles/index.css';
-=======
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { routes } from './routes';
->>>>>>> main
-
-import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
