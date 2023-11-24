7c6cd62df3b7fee147733fb1d5a46ff0dbb3b2b5
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2023-11-23 22:03:27 -0300

    Revert "Merge branch 'gabriel' into Johann"
    This reverts commit a24df4325a6e692c9903c744e03478a40e92ba75, reversing
    changes made to 5fe289c5a1a27589c2d39aa971982f18852511cf.

--- easy-party/src/routes.js
+++ easy-party/src/routes.js
@@ -1,21 +1,17 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter} from 'react-router-dom';
+
 import LoginPage from './Pages/LoginPage/LoginPage';
 import Index from './Pages/Index/Index';
-import AddEventPage from './Pages/AddEvent/AddEventPage'; // Alterado o nome do identificador
 
 const routes = createBrowserRouter([
     {
-        path: "/home",
-        element: (<Index />)
+        path:"/home",
+        element:(<Index/>)
     },
     {
-        path: "/",
-        element: (<LoginPage />)
+        path:"/",
+        element:(<LoginPage/>)
     },
-    {
-        path: "/addevent",
-        element: (<AddEventPage />)
-    },
-]);
+])
 
-export { routes };
+export {routes};
